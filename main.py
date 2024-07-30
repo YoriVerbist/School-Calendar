1e15e4277d3a32f07ed4da9db89f710768515928
Yori Verbist <yori.verbist12@gmail.com>
2024-07-30 19:04:59 +0200

    Fetched all the course names

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ def main():
 
     main_section_titles = find_element(soup, "h3", {"class", "mandatory"})
 
-    lis = soup.find_all("li", {"class", "lilevel_2"})
-    for li in lis:
-        titles = find_element(li, "h4", {"class", "mandatory"})
-        print(titles)
+    lis_level2 = soup.find_all("li", {"class", "lilevel_2"})
+    titles_level2 = []
+    for li in lis_level2:
+        title_mandatory = find_element(li, "h4", {"class", "mandatory"})
+        if title_mandatory:
+            titles_level2.append(title_mandatory)
+        title_optional = find_element(li, "h4", {"class", "optional"})
+        if title_optional:
+            titles_level2.append(title_optional)
+    print(titles_level2)
+
+    lis_level3 = soup.find_all("li", {"class", "lilevel_3"})
+    titles_level3 = []
+    for li in lis_level3:
+        course_name = find_element(li, "td", {"class", "opleidingsonderdeel"})
+        if course_name:
+            titles_level3.append(course_name)
+    print(titles_level3)
 
 
 def find_element(soup, h_element: str, args: dict):
